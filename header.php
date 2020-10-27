d0cbebd05b79b106a61dad353240d200dcb3d5ac
Krzysztof Stambula <chris@mono.coffee>
2020-10-27 12:05:40 +0000

    added container to header

--- header.php
+++ header.php
@@ -11,39 +11,35 @@
 
 <body <?php body_class() ?>>
 <header class="site-header">
-    <div class="site-logo">
-        <?php echo get_custom_logo(); ?>
+    <div class="container">
+	<div class="site-logo">
+	    <?php echo get_custom_logo(); ?>
+	</div>
+	<?php if ( has_nav_menu( 'primary' )) : ?>
+	    <nav role="navigation">
+		<ul class="primary-menu">
+		    <?php wp_nav_menu(
+			array(
+			    'container' => '',
+			    'items_wrap' => '%3$s',
+			    'theme_location' => 'primary',
+			)
+		    ); ?>
+		</ul>
+	    </nav>
+	<?php endif; ?>
+	<?php if ( has_nav_menu( 'lang' )) : ?>
+	    <nav>
+		<ul class="lang-menu">
+		    <?php wp_nav_menu(
+			array(
+			    'container' => '',
+			    'items_wrap' => '%3$s',
+			    'theme_location' => 'lang',
+			)
+		    ); ?>
+		</ul>
+	    </nav>
+	<?php endif; ?>
     </div>
-        <?php
-        if ( has_nav_menu( 'primary' )) :
-        ?>
-        <nav role="navigation">
-            <ul class="primary-menu">
-                <?php
-                wp_nav_menu(
-                    array(
-                        'container' => '',
-                        'items_wrap' => '%3$s',
-                        'theme_location' => 'primary',
-                    )
-                );
-                ?>
-            </ul>
-        </nav>
-        <?php endif; ?>
-        <?php if ( has_nav_menu( 'lang' )) : ?>
-        <nav>
-            <ul class="lang-menu">
-                <?php
-                wp_nav_menu(
-                    array(
-                        'container' => '',
-                        'items_wrap' => '%3$s',
-                        'theme_location' => 'lang',
-                    )
-                );
-                ?>
-            </ul>
-        </nav>
-        <?php endif; ?>
 </header>
