6b6b15158f36cd2d51d37b7278ddd95ef2a4931d
Krzysztof Stambula <chris@mono.coffee>
2020-10-27 13:10:31 +0000

    add blank page template for use with landing pages

--- header.php
+++ header.php
@@ -1,15 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?> xmlns:fb="http://ogp.me/ns/fb#">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no" />
-<link rel="profile" href="https://gmpg.org/xfn/11">
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700;800&display=swap" rel="stylesheet">
-<?php wp_head(); ?>
-<script src="https://kit.fontawesome.com/3aaeb33305.js" crossorigin="anonymous"></script>
-</head>
-
-<body <?php body_class() ?>>
+<?php include('common/header.php'); ?>
 <header class="site-header">
     <div class="container">
 	<div class="site-logo">
